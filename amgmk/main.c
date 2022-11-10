419a14bc8d93a0b32c1f6ac0e23a94cb8a7685bf
ianbig <ian825747@gmail.com>
2022-11-10 14:54:32 -0500

    submission

--- amgmk/main.c
+++ amgmk/main.c
@@ -7,34 +7,30 @@
  *EHEADER****************************************************************/
 
 //--------------
-//  A micro kernel 
+//  A micro kernel
 //--------------
 #include <stdio.h>
 #include <stdlib.h>
-#include "omp.h"
 
 #include "headers.h"
-
+#include "omp.h"
 
 //
-const int testIter   = 1000;
+const int testIter = 1000;
 double totalWallTime = 0.0;
 
-// 
+//
 void test_Matvec();
 void test_Relax();
 void test_Axpy();
 
 //
-int main(int argc, char *argv[])
-{
-  double t0        = 0.0,
-         t1        = 0.0,
-         del_wtime = 0.0;
+int main(int argc, char *argv[]) {
+  double t0 = 0.0, t1 = 0.0, del_wtime = 0.0;
 
-  int  max_num_threads;
+  int max_num_threads;
 
-  //omp_set_num_threads(4);
+  omp_set_num_threads(4);
 
   printf("\n");
   printf("//------------ \n");
@@ -43,20 +39,18 @@ int main(int argc, char *argv[])
   printf("// \n");
   printf("//------------ \n");
 
- 
-  #pragma omp parallel
-     #pragma omp master
-        max_num_threads = omp_get_num_threads();
-   printf("\nmax_num_threads = %d \n\n",max_num_threads );
+#pragma omp parallel
+#pragma omp master
+  max_num_threads = omp_get_num_threads();
+  printf("\nmax_num_threads = %d \n\n", max_num_threads);
 
-   printf("\n testIter   = %d \n\n", testIter );  
+  printf("\n testIter   = %d \n\n", testIter);
 
   t0 = omp_get_wtime();
 
-
   // Matvec
   totalWallTime = 0.0;
- 
+
   test_Matvec();
 
   printf("\n");
@@ -67,7 +61,6 @@ int main(int argc, char *argv[])
   printf("//------------ \n");
 
   printf("\nWall time = %f seconds. \n", totalWallTime);
-
 
   // Relax
   totalWallTime = 0.0;
@@ -83,10 +76,9 @@ int main(int argc, char *argv[])
 
   printf("\nWall time = %f seconds. \n", totalWallTime);
 
-
   // Axpy
   totalWallTime = 0.0;
- 
+
   test_Axpy();
 
   printf("\n");
@@ -98,21 +90,18 @@ int main(int argc, char *argv[])
 
   printf("\nWall time = %f seconds. \n", totalWallTime);
 
-  t1 = omp_get_wtime();;
+  t1 = omp_get_wtime();
+  ;
 
   del_wtime = t1 - t0;
 
   printf("\nTotal Wall time = %f seconds. \n", del_wtime);
 
-
-  return  0;
-
+  return 0;
 }
 
-void test_Matvec()
-{
-  double t0 = 0.0,
-         t1 = 0.0;
+void test_Matvec() {
+  double t0 = 0.0, t1 = 0.0;
 
   hypre_CSRMatrix *A;
   hypre_Vector *x, *y, *sol;
@@ -121,39 +110,36 @@ void test_Matvec()
   double *y_data, *sol_data;
   double error, diff;
 
-  nx = 50;  /* size per proc nx*ny*nz */
+  nx = 50; /* size per proc nx*ny*nz */
   ny = 50;
   nz = 50;
 
   values = hypre_CTAlloc(double, 4);
-  values[0] = 6; 
+  values[0] = 6;
   values[1] = -1;
   values[2] = -1;
   values[3] = -1;
 
   A = GenerateSeqLaplacian(nx, ny, nz, values, &y, &x, &sol);
 
-  hypre_SeqVectorSetConstantValues(x,1);
-  hypre_SeqVectorSetConstantValues(y,0);
+  hypre_SeqVectorSetConstantValues(x, 1);
+  hypre_SeqVectorSetConstantValues(y, 0);
 
   t0 = omp_get_wtime();
-  for (i=0; i<testIter; ++i)
-      hypre_CSRMatrixMatvec(1,A,x,0,y);
-  t1 = omp_get_wtime() ;
+  for (i = 0; i < testIter; ++i) hypre_CSRMatrixMatvec(1, A, x, 0, y);
+  t1 = omp_get_wtime();
 
   totalWallTime += t1 - t0;
 
- 
   y_data = hypre_VectorData(y);
   sol_data = hypre_VectorData(sol);
 
   error = 0;
-  for (i=0; i < nx*ny*nz; i++)
-  {
-      diff = fabs(y_data[i]-sol_data[i]);
-      if (diff > error) error = diff;
+  for (i = 0; i < nx * ny * nz; i++) {
+    diff = fabs(y_data[i] - sol_data[i]);
+    if (diff > error) error = diff;
   }
-     
+
   if (error > 0) printf(" \n Matvec: error: %e\n", error);
 
   hypre_TFree(values);
@@ -161,13 +147,10 @@ void test_Matvec()
   hypre_SeqVectorDestroy(x);
   hypre_SeqVectorDestroy(y);
   hypre_SeqVectorDestroy(sol);
-
 }
 
-void test_Relax()
-{
-  double t0 = 0.0,
-         t1 = 0.0;
+void test_Relax() {
+  double t0 = 0.0, t1 = 0.0;
 
   hypre_CSRMatrix *A;
   hypre_Vector *x, *y, *sol;
@@ -176,35 +159,33 @@ void test_Relax()
   double *x_data;
   double diff, error;
 
-  nx = 50;  /* size per proc nx*ny*nz */
+  nx = 50; /* size per proc nx*ny*nz */
   ny = 50;
   nz = 50;
 
   values = hypre_CTAlloc(double, 4);
-  values[0] = 6; 
+  values[0] = 6;
   values[1] = -1;
   values[2] = -1;
   values[3] = -1;
 
   A = GenerateSeqLaplacian(nx, ny, nz, values, &y, &x, &sol);
 
-  hypre_SeqVectorSetConstantValues(x,1);
+  hypre_SeqVectorSetConstantValues(x, 1);
 
   t0 = omp_get_wtime();
-  for (i=0; i<testIter; ++i)
-      hypre_BoomerAMGSeqRelax(A, sol, x);
+  for (i = 0; i < testIter; ++i) hypre_BoomerAMGSeqRelax(A, sol, x);
   t1 = omp_get_wtime();
 
   totalWallTime += t1 - t0;
 
   x_data = hypre_VectorData(x);
   error = 0;
-  for (i=0; i < nx*ny*nz; i++)
-  {
-      diff = fabs(x_data[i]-1);
-      if (diff > error) error = diff;
+  for (i = 0; i < nx * ny * nz; i++) {
+    diff = fabs(x_data[i] - 1);
+    if (diff > error) error = diff;
   }
-     
+
   if (error > 0) printf(" \n Relax: error: %e\n", error);
 
   hypre_TFree(values);
@@ -212,22 +193,18 @@ void test_Relax()
   hypre_SeqVectorDestroy(x);
   hypre_SeqVectorDestroy(y);
   hypre_SeqVectorDestroy(sol);
-
 }
 
-void test_Axpy()
-{
-  double t0 = 0.0,
-         t1 = 0.0;
-
+void test_Axpy() {
+  double t0 = 0.0, t1 = 0.0;
 
   hypre_Vector *x, *y;
   int nx, i;
-  double alpha=0.5;
+  double alpha = 0.5;
   double diff, error;
   double *y_data;
 
-  nx = 125000;  /* size per proc  */
+  nx = 125000; /* size per proc  */
 
   x = hypre_SeqVectorCreate(nx);
   y = hypre_SeqVectorCreate(nx);
@@ -235,30 +212,24 @@ void test_Axpy()
   hypre_SeqVectorInitialize(x);
   hypre_SeqVectorInitialize(y);
 
-  hypre_SeqVectorSetConstantValues(x,1);
-  hypre_SeqVectorSetConstantValues(y,1);
+  hypre_SeqVectorSetConstantValues(x, 1);
+  hypre_SeqVectorSetConstantValues(y, 1);
 
- 
   t0 = omp_get_wtime();
-  for (i=0; i<testIter; ++i)
-      hypre_SeqVectorAxpy(alpha,x,y);
+  for (i = 0; i < testIter; ++i) hypre_SeqVectorAxpy(alpha, x, y);
   t1 = omp_get_wtime();
-  
 
   y_data = hypre_VectorData(y);
   error = 0;
-  for (i=0; i < nx; i++)
-  {
-    diff = fabs(y_data[i]-1-0.5*(double)testIter);
-      if (diff > error) error = diff;
+  for (i = 0; i < nx; i++) {
+    diff = fabs(y_data[i] - 1 - 0.5 * (double)testIter);
+    if (diff > error) error = diff;
   }
-     
+
   if (error > 0) printf(" \n Axpy: error: %e\n", error);
 
-  totalWallTime += t1 - t0; 
+  totalWallTime += t1 - t0;
 
   hypre_SeqVectorDestroy(x);
   hypre_SeqVectorDestroy(y);
-
 }
-
